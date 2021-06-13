de8693afb5569c78bab20f623c748945047a1bb2
lllrrr2 <61368564+lllrrr2@users.noreply.github.com>
2021-06-13 21:05:51 +0800

    mod 19.07

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #0d0ab01a64f25d15325d3c98a7df252085382f1d  19.07.4  maketool不同
 #51f55b58b75b189f4f8810a827ca27a959e2192d  19.07.5 同7 target.mk/maketool
 #6ba9740b61a3ca89355ae1b08f0456617e0ce3c0  19.07.6 同7 target.mk/maketool
-sed -i 's/luci.git^34e0d656a41befd9720be35c4831c9f136a67c59/luci.git;openwrt-18.06/g' feeds.conf.default
+sed -i 's/luci.git;openwrt-19.07/luci.git;openwrt-18.06/g' feeds.conf.default
 
 # Lienol/openwrt-package
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
